0bacc9c534cbf5b32cf055a5bf25c1dffc815eb7
BenCDA <cardosobenjamin01@gmail.com>
2023-06-01 09:28:54 +0200

    end champs page

--- Champs/script.js
+++ Champs/script.js
@@ -3,19 +3,72 @@ document.addEventListener('DOMContentLoaded', () => {
   const searchInput = document.getElementById('search-input');
   const championList = document.getElementById('champion-list');
   const championDetails = document.getElementById('champion-details');
-  const championModal = document.getElementById('champion-modal');
   const championName = document.getElementById('champion-name');
-  const championImage = document.getElementById('champion-image');
+  const championDetailsImage = document.getElementById('champion-details-image');
   const championSkills = document.getElementById('champion-skills');
   const championStats = document.getElementById('champion-stats');
-  const closeButton = document.getElementsByClassName('close')[0];
+  const championModal = document.getElementById('champion-modal');
+  const championModalName = document.getElementById('champion-modal-name');
+  const championModalImage = document.getElementById('champion-modal-image');
+  const championModalSkills = document.getElementById('champion-modal-skills');
+  const championModalStats = document.getElementById('champion-modal-stats');
+  const closeBtn = document.getElementsByClassName('close')[0];
 
   let champions = [];
 
-  // Function to display champion details
+  // Function to display champion details in modal
   const displayChampionDetails = (championData) => {
+    championModalName.textContent = championData.name;
+    championModalImage.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championData.id}_0.jpg`;
+
+    // Clear previous skills and stats
+    championModalSkills.innerHTML = '';
+    championModalStats.innerHTML = '';
+
+    if (championData.spells && Array.isArray(championData.spells)) {
+      championData.spells.forEach(spell => {
+        const skillImage = document.createElement('img');
+        skillImage.src = `https://ddragon.leagueoflegends.com/cdn/${championData.version}/img/spell/${spell.image.full}`;
+        championModalSkills.appendChild(skillImage);
+      });
+    }
+
+    const statsChart = document.createElement('canvas');
+    statsChart.width = 400;
+    statsChart.height = 400;
+    championModalStats.appendChild(statsChart);
+
+    const statsLabels = Object.keys(championData.stats);
+    const statsValues = Object.values(championData.stats);
+
+    new Chart(statsChart, {
+      type: 'line',
+      data: {
+        labels: statsLabels,
+        datasets: [{
+          label: 'Stats',
+          data: statsValues,
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+    championModal.style.display = 'block'; // Show champion modal
+  };
+
+  // Function to display champion details in the sidebar
+  const displayChampion = (championData) => {
     championName.textContent = championData.name;
-    championImage.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championData.id}_0.jpg`;
+    championDetailsImage.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championData.id}_0.jpg`;
 
     // Clear previous skills and stats
     championSkills.innerHTML = '';
@@ -57,70 +110,69 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       }
     });
-
-    championModal.style.display = 'block'; // Show champion details
   };
 
-  // Function to create champion elements and attach event listeners
-  const createChampionElements = (championsData) => {
-    championList.innerHTML = ''; // Clear previous champion elements
-
-    championsData.forEach(championData => {
-      const championDiv = document.createElement('div');
-      championDiv.classList.add('champion');
-
-      const championNameElement = document.createElement('h3');
-      championNameElement.textContent = championData.name;
-
-      const championImageElement = document.createElement('img');
-      championImageElement.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championData.id}_0.jpg`;
-
-      championDiv.appendChild(championNameElement);
-      championDiv.appendChild(championImageElement);
-
-      championDiv.addEventListener('click', () => {
-        displayChampionDetails(championData);
-      });
-
-      championList.appendChild(championDiv);
-    });
-  };
-
-  // Function to filter champions by name, skill, or lore
-  const filterChampions = (searchTerm) => {
-    const filteredChampions = champions.filter(champion => {
-      const nameMatch = champion.name.toLowerCase().includes(searchTerm.toLowerCase());
-      const skillMatch = champion.spells && champion.spells.some(spell => spell.name.toLowerCase().includes(searchTerm.toLowerCase()));
-      const loreMatch = champion.lore && champion.lore.toLowerCase().includes(searchTerm.toLowerCase());
-
-      return nameMatch || skillMatch || loreMatch;
-    });
-
-    createChampionElements(filteredChampions);
-  };
-
-  // Add event listener for search input
-  searchInput.addEventListener('input', (event) => {
-    const searchTerm = event.target.value;
-    filterChampions(searchTerm);
-  });
-
-  // Close the modal
-  closeButton.onclick = () => {
+  // Function to close the champion modal
+  const closeModal = () => {
     championModal.style.display = 'none';
   };
 
-  // Load champions from Riot API
-  fetch('https://ddragon.leagueoflegends.com/api/versions.json')
-    .then(response => response.json())
-    .then(versions => {
-      const latestVersion = versions[0];
+  // Event listener for the close button
+  closeBtn.addEventListener('click', closeModal);
 
-      fetch(`https://ddragon.leagueoflegends.com/cdn/${latestVersion}/data/fr_FR/champion.json`)
-        .then(response => response.json())
-        .then(data => {
-          champions = Object.values(data.data);
-          createChampionElements(champions);
+  // Fetch champion data from the API
+  fetch('https://ddragon.leagueoflegends.com/cdn/11.11.1/data/en_US/champion.json')
+    .then(response => response.json())
+    .then(data => {
+      champions = Object.values(data.data);
+
+      // Display champions in the sidebar
+      championList.innerHTML = '';
+      champions.forEach(champion => {
+        const championElement = document.createElement('div');
+        championElement.classList.add('champion');
+        championElement.innerHTML = `
+          <img class="champion-image" src="https://ddragon.leagueoflegends.com/cdn/11.11.1/img/champion/${champion.image.full}" alt="Champion Image">
+          <h3 class="champion-name">${champion.name}</h3>
+        `;
+        championElement.style.width = '180px'; // Set the width of champion element
+        championElement.style.height = '220px'; // Set the height of champion element
+        championList.appendChild(championElement);
+
+        // Add click event listener to champion element
+        championElement.addEventListener('click', () => {
+          displayChampionDetails(champion);
         });
+      });
     });
+
+  // Function to filter and display champions based on search input
+  const filterChampions = () => {
+    const searchValue = searchInput.value.toLowerCase();
+    const filteredChampions = champions.filter(champion =>
+      champion.name.toLowerCase().includes(searchValue)
+    );
+
+    // Display filtered champions in the sidebar
+    championList.innerHTML = '';
+    filteredChampions.forEach(champion => {
+      const championElement = document.createElement('div');
+      championElement.classList.add('champion');
+      championElement.innerHTML = `
+        <img class="champion-image" src="https://ddragon.leagueoflegends.com/cdn/11.11.1/img/champion/${champion.image.full}" alt="Champion Image">
+        <h3 class="champion-name">${champion.name}</h3>
+      `;
+      championElement.style.width = '180px'; // Set the width of champion element
+      championElement.style.height = '220px'; // Set the height of champion element
+      championList.appendChild(championElement);
+
+      // Add click event listener to champion element
+      championElement.addEventListener('click', () => {
+        displayChampionDetails(champion);
+      });
+    });
+  };
+
+  // Search champion event listener
+  searchInput.addEventListener('input', filterChampions);
 });
